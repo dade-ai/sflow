12688c12f105e062babe58e885f42bfa3fdb2f2d
dade-ai <aiplore@gmail.com>
2017-07-04 16:12:39 +0900

    setup.py

---
+++ README.rst
@@ -0,0 +1,12 @@
+sflow
+===
+
+use@your.own.risk
+
+example
+---
+
+```python
+import sflow.tf as tf
+```
+
---
+++ setup.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+"""
+reference : https://github.com/pypa/sampleproject/blob/master/setup.py
+"""
+
+from setuptools import (setup, find_packages)
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(name='sflow',
+      version='1.2.0',
+      description='super slim and simple syntatic sugar for Tensorflow',
+      long_description='텐서플로우 개취 탑재',
+
+      # The project's main homepage.
+      url='https://github.com/dade-ai/sflow',
+      author='dade',
+      author_email='aiplore@gmail.com',
+
+      license='MIT',
+
+      # The project's main homepage.
+
+      # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          # How mature is this project? Common values are
+          #   3 - Alpha
+          #   4 - Beta
+          #   5 - Production/Stable
+          'Development Status :: 3 - Alpha',
+
+          # Indicate who your project is intended for
+          'Intended Audience :: Developers',
+
+          # Pick your license as you wish (should match "license" above)
+          'License :: OSI Approved :: MIT License',
+
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+      ],
+
+      packages=find_packages(),
+      install_requires=[
+          'tensorflow',
+          'snipy'
+      ])
